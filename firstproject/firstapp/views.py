39c8ba532530c09a6684b91b8d3817da01ce9aa9
Deepesh70 <deepeshdangi700@gmail.com>
2026-06-19 19:34:51 +0530

    learning project setup

---
+++ firstproject/firstapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .forms import ReservationForms
+
+# Create your views here.
+
+def hello_world(request):
+    return HttpResponse("Hello World")
+
+class HelloEthiopia(View):
+    def get(self, request):
+        return HttpResponse("Hello Ethiopia")
+
+def home(request):
+    form = ReservationForms()
+
+    if request.method == 'POST':
+        form = ReservationForms(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Reservation saved successfully!")
+    return render(request, 'index.html', {'form': form })    
